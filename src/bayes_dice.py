93e721601f8cba319fbed7e95fba9d3ea2726b22
gabrielestes <gabriel.estes@mckesson.com>
2018-07-27 15:41:17 -0400

    create choose die method and test

--- src/bayes_dice.py
+++ src/bayes_dice.py
@@ -1,2 +1,8 @@
+import random
+
 class BayesDice:
-    pass
+    def __init__(self):
+        self.dice = [4, 6, 8, 12, 20]
+
+    def choose_die(self):
+        return random.choice(self.dice)
--- test/test_bayes_dice.py
+++ test/test_bayes_dice.py
@@ -3,3 +3,7 @@ from src.bayes_dice import BayesDice
 def test_class_works():
     bd = BayesDice()
     assert isinstance(bd, BayesDice)
+
+def test_choose_die():
+    bd = BayesDice()
+    assert bd.choose_die() in bd.dice
