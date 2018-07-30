00960221e37b61b01cb4aa943c01852c819a9d39
gabrielestes <gabriel.estes@mckesson.com>
2018-07-30 14:14:43 -0500

    revisions after guided lesson

--- src/bayes_dice.py
+++ src/bayes_dice.py
@@ -3,20 +3,30 @@ import numpy as np
 class BayesDice:
     def __init__(self):
         self.dice = [4, 6, 8, 12, 20]
-        self.die = None
+        self.die = 0
+        prior = 0.20
+        self.data = {die: prior for die in self.dice}
 
     def choose_die(self):
         self.die = random.choice(self.dice)
         return self.die
 
     def roll_die(self):
-        roll = random.choice(range(1, self.dice + 1)) # roll the die and get a random number from the die range
+        roll = random.randint(1, self.dice + 1) # roll the die and get a random number from the die range
+        update_priors(roll)
         return (bayes_theorem(roll)) # execute bayes theorem once and set to posterior, P(B|A)
 
     def bayes_theorem(self, roll):
         return (1/self.die) * .2 / (prob_of_roll(self.die))
 
-    def prob_of_roll(self, roll):
-        valid_dice = [die for die in self.dice if roll <= die]
-        each_prob = sum([1/die for die in valid_dice])/50
-        options = (valid_dice.count())/50
+    def update_priors(self, roll):
+        denominator = list(map(lambda die: 0 if roll > die else (1/die) * self.data[die], self.dice)) # P(B)
+        for i in range(len(self.dice)):
+            die = self.dice[i]
+            numerator = denominator[i]
+            posterior = numerator / denomenator
+            self.data[die] = posterior
+            print('die:', self.die, 'roll:', roll)
+            for die, prior in self.data.items():
+                print("{}: {:.2f}".format(die, prior))
+            print('\n')
